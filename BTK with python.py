99490bcf83bfc8299938219991a62a8e26ca67b4
Emily Skidmore <emily.skidmore.2024@uni.strath.ac.uk>
2025-05-27 16:04:03 +0100

    exploring btk functions, finding no.frames, frequency and duration of run

--- BTK with python.py
+++ BTK with python.py
@@ -1,19 +1,34 @@
 #Trying to use BTK to read cd3 running file
-
+# pip install moveck-bridge-btk - type into terminal to install package
 #Importing the BTK package
 import moveck_bridge_btk as btk
 
 # Check the available functions and classes int he btk package
 print(dir(btk))
 
-#reader = btk.btkReadAcquisition(r'C:\Users\kwb24166\OneDrive - University of Strathclyde\Documents\Project\Python\Data\run.c3d')
-
 try:
-    acquisition = btk.btkReadAcquisition(r'C:\Users\kwb24166\OneDrive - University of Strathclyde\Documents\Project\Python\Data\run.c3d')
-    print("acquisition data loaded successfully")
-    print(acquisition)
+    Read_Data = btk.btkReadAcquisition(r'C:\Users\kwb24166\OneDrive - University of Strathclyde\Documents\Project\Python\Data\run.c3d')
+    print("Data loaded successfully")
+    print(Read_Data)
 except Exception as e:
     print("error:",e)
 
-markers = btk.btkGetMarkers(acquisition)
-print("Markers", markers)
+Marker_Data = btk.btkGetMarkers(Read_Data)
+print("Markers", Marker_Data)
+print(Marker_Data, [1,1])
+
+time_0 = btk.btkGetFirstFrame(Read_Data)
+print(time_0)
+time_t = btk.btkGetLastFrame(Read_Data)
+print(time_t)
+
+no_frames = btk.btkGetPointFrameNumber(Read_Data)
+print(f"Number of frames - {no_frames}")
+no_data_points = btk.btkGetPointNumber(Read_Data)
+print(no_data_points)
+frame_freq = btk.btkGetPointFrequency(Read_Data)
+print(f"Frame rate - {frame_freq}")
+Duration = no_frames/ frame_freq
+print(f"Run duration - {Duration} seconds")
+pointsInfo = btk.btkGetPoints(Read_Data)
+pointsInfo
