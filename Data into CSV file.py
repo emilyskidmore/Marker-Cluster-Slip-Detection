f86f5d75d9cbe54514f206f09dad1f3f27d6eae1
Millie Skidmore <s1929340@ed.ac.uk>
2025-05-29 16:22:00 +0100

    Adding code comments and trying to get a marker trajectory

--- Data into CSV file.py
+++ Data into CSV file.py
@@ -1,21 +1,31 @@
 import ezc3d
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import moveck_bridge_btk as btk
 
-# Load C3D
+
+# Load data from cd3 file
 c3d = ezc3d.c3d('run.c3d')
 
-# Extract marker data and labels
-marker_data = c3d['data']['points']  # Shape: (4, num_markers, num_frames)
-labels = c3d['parameters']['POINT']['LABELS']['value']
+# Extract marker data in the form of a numpy array that contains the marker coordinates
+# The following extracts the marker positions ['points'] from the numerical data ['data'] of the file 
+marker_data = c3d['data']['points']
+# Finding the shape of the numpy array - typically it is (4, no.markers, no.frames)
+# The first dimension is often 4 [x,y,z,residual]
+marker_data.shape 
+print(f"The data has the shape {marker_data.shape} which means it contains x,y,z coordinates and a residual, {marker_data.shape[1]} markers and {marker_data.shape[2]} frames")
+# The following accesses the parameters section of the c3d file, then into 'point' which contains information about the markers, then into 'labels' which is the names of the markers used, then finally into 'values' which is a string of the marker labels
+Marker_labels = c3d['parameters']['POINT']['LABELS']
+print(Marker_labels)
 
-# Get only XYZ (ignore 4th row: camera residuals)
-xyz_data = marker_data[:3, :, :]  # Shape: (3, num_markers, num_frames)
-
-# Transpose and reshape to DataFrame
-# Resulting shape will be (num_frames * num_markers, 5)
-num_frames = xyz_data.shape[2]
-num_markers = xyz_data.shape[1]
+# Get only x,y,z coordinates, ignoring the residual
+marker_data = marker_data[:3, :, :]  # The shape becomes (3,no.markers, no.frames)
+print(f"The data has the shape {marker_data.shape} which means it contains x,y,z coordinates, {marker_data.shape[1]} markers and {marker_data.shape[2]} frames")
+no_markers = marker_data.shape[1]
+no_frames = marker_data.shape[2]
+print(f"The number of markers is {no_markers}")
+print(f"The number of frames is {no_frames}")
 
 # Create a DataFrame
 records = []
@@ -32,3 +42,25 @@ for frame_idx in range(num_frames):
 
 df = pd.DataFrame.from_records(records)
 df.to_csv("Data.csv")
+print(labels)
+
+L_Thigh = 'LLTHI'
+L_Thigh_data = df[df['Marker']== L_Thigh]
+print(L_Thigh_data)
+
+X = L_Thigh_data['X']
+Y = L_Thigh_data['Y']
+Z = L_Thigh_data['Z']
+print(X)
+print(Y)
+print(Z)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='2d')
+ax.plot3D(marker_df['X'], marker_df['Y'], marker_df['Z'], label='LLTHI', color='blue')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title(f"Trajectory of Marker: {L_Thigh}")
+ax.legend()
+plt.show()
